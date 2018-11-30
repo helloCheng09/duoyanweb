9009c9265b10db2aa14e350247a6776db4e9e47a
po_chen <1814638551@qq.com>
2018-11-30 09:40:22 +0800

    update 20181139

--- src/js/index.js
+++ src/js/index.js
@@ -35,6 +35,26 @@
       })
     }
 
+    // 缓冲运动
+    function startMove(dom, obj) {
+      clearInterval(dom.timer)
+      var target = obj.target
+      // 判断方向
+      dom.timer = setInterval(() => {
+        iSpeed = (target - dom.offsetTop) / 4
+        //..
+        iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed)
+        if (dom.offsetTop == target) {
+          clearInterval(dom.timer)
+        } else {
+          dom.style.top = dom.offsetTop + iSpeed + "px"
+        }
+      }, 20);
+
+
+    }
+
+    root.startMove = startMove
     root.TagToggle = TagToggle
   }(window.$, window.dyweb || (window.dyweb = {})));
 
@@ -87,7 +107,7 @@
         "cover": "http://www.wangjiao.com/data/upload/2018/1121/17/5bf52adbcfd74_266_170.jpg",
         "rePlay": false,
         "playsinline": true,
-        "preload": true,
+        "preload": false,
         "controlBarVisibility": "hover",
         "useH5Prism": true,
         "skinLayout": [{
@@ -159,8 +179,6 @@
       }, function (player) {
         console.log("播放器创建了。");
       });
-
-
     } else if (document.getElementById('centerNews')) {
       // 新闻中心
       root.TagToggle(".fenlei-list-b .list-item", ".item-text")
@@ -179,8 +197,7 @@
         "preload": false,
         "controlBarVisibility": "hover",
         "useH5Prism": true,
-        "skinLayout": [
-          {
+        "skinLayout": [{
             "name": "bigPlayButton",
             "align": "blabs",
             "x": 30,
@@ -213,8 +230,7 @@
             "align": "blabs",
             "x": 0,
             "y": 0,
-            "children": [
-              {
+            "children": [{
                 "name": "progress",
                 "align": "blabs",
                 "x": 0,
@@ -248,9 +264,78 @@
           }
         ]
       }, function (player) {
-          console.log("播放器创建了。");
-        }
-      );
+        console.log("播放器创建了。");
+      });
+      // 实例化轮播
+      var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 2,
+        spaceBetween: 10,
+        slidesPerGroup: 2,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+
+    } else if (document.getElementById('centerPro')) {
+      // 产品中心
+      // 模块动效
+      var oDom = document.getElementsByClassName('mark-down')
+      $(".pro-item").each(function () {
+        var i = $(this).index()
+        var dom = oDom[i]
+        $(this).find(".img-b").hover(function () {
+          if (dom.offsetTop >= "-300") {
+            root.startMove(dom, {
+              target: 0,
+            })
+          }
+        }, function (i) {
+          if (dom.offsetTop <= "0") {
+            root.startMove(dom, {
+              target: -300,
+            })
+          }
+        })
+      })
+
+      // 实例化轮播
+      var mySwiper = new Swiper('#swiper1', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        initialSlide: 1,
+        loop: true,
+        coverflowEffect: {
+          rotate: 0,
+          stretch: 0,
+          depth: 81,
+          modifier: 3,
+          slideShadows: false,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+
+      })
+
+    } else if (document.getElementById('jionUs')) {
+      // 招商加盟
+
+    } else if (document.getElementById('contactUs')) {
+      // 联系我们
+      /* 地图组件 */
+      var map = new BMap.Map("container");
+      // 创建地图实例  
+      var point = new BMap.Point(117.34897,31.980139);
+      // 创建点坐标  
+      map.centerAndZoom(point, 15);
+      var marker = new BMap.Marker(point); // 创建标注    
+      map.addOverlay(marker); // 将标注添加到地图中
     }
   }(window.$, window.dyweb || (window.dyweb = {})));
 
